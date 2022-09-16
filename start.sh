0cb9921aba360fd59ae577e2a7034b9ab0684ff6
Noah Osuolale <osuolale49@gmail.com>
2022-09-16 14:29:11 +0100

    Created Laravel birdboard app with elb deployment

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'Waiting for Mysql to be available'
+maxTries=10
+while [ ${maxTries} -gt 0 ] && ! mysql --user=${DB_USERNAME} --password=${DB_PASSWORD} --host=${DB_HOST} --port=${DB_PORT} --database=${DB_DATABASE} -e exit; do
+   sleep 15
+done
+
+php artisan optimize
+php artisan migrate --force
+php-fpm
